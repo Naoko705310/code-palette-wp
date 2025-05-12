84e16c1cf454450b33413496b166a336d7ec32a9
Naoko705310 <naokos79@gmail.com>
2025-05-13 01:09:52 +0900

    管理バーを非表示に

--- CodePaletteTheme/functions.php
+++ CodePaletteTheme/functions.php
@@ -72,7 +72,7 @@ add_action('wp_enqueue_scripts', 'add_theme_scripts');
 /* --------------------------------------------
 /* // ログイン中でも管理バーを非表示にする
 /* -------------------------------------------- */
-// add_filter('show_admin_bar', '__return_false');
+add_filter('show_admin_bar', '__return_false');
 
 
 /* --------------------------------------------
